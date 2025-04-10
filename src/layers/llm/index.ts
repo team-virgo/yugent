faabbfba921bc5605b114e2af677c19b1726a5a2
mahesh <maheshmahi19@outlook.com>
2025-04-11 03:15:43 +0530

    chore: providers enum

--- src/layers/llm/index.ts
+++ src/layers/llm/index.ts
@@ -1,3 +1,13 @@
 export * from "./openai";
 export * from "./mistral";
 export * from "./groq";
+
+export const OPENAI = "openai";
+export const GROQ = "GROQ";
+export const MISTRAL = "MISTRAL";
+
+export enum Providers {
+  OPENAI = "openai",
+  GROQ = "groq",
+  MISTRAL = "mistral",
+}
